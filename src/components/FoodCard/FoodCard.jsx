f4af898c906a9812aa16e7f1798be0e2473b6129
Minhajul-alam <alam.minhajul@outlook.com>
2023-12-14 23:01:42 +0600

    Update menu list

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -56,7 +56,7 @@ const FoodCard = ({ item }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={image} alt="Shoes" />
+        <img src={image} alt="" />
       </figure>
       <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
         ${price}
