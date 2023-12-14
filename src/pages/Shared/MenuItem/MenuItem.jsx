f7acadba5d7cb4d955171492b0828fc334d095ac
Minhajul-alam <alam.minhajul@outlook.com>
2023-12-14 23:04:43 +0600

    Update menu items image shap

--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -3,13 +3,12 @@ const MenuItem = ({ item }) => {
   return (
     <div className="flex space-x-2">
       <img
-        style={{ borderRadius: "0 200px 200px 200px" }}
-        className="w-[100px]"
+        className="w-[100px] rounded-full"
         src={image}
         alt=""
       />
       <div>
-        <h3 className="uppercase">{name}----------</h3>
+        <h3 className="uppercase">{name}</h3>
         <p>{recipe}</p>
       </div>
       <p className="text-yellow-500">${price}</p>
